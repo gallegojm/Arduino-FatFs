19d7a159f6a9bcf376081e3df4d96dd63f952d20
Alessandro Francesconi <alessandrofrancesconi@live.it>
2021-08-31 19:14:10 +0200

    Fixed wrong merge

--- FatFs/src/diskio.c
+++ FatFs/src/diskio.c
@@ -1,5 +1,5 @@
 /*-----------------------------------------------------------------------*/
-/* Low level disk I/O module SKELETON for FatFs     (C)ChaN, 2019        */
+/* Low level disk I/O module skeleton for FatFs     (C)ChaN, 2019        */
 /*-----------------------------------------------------------------------*/
 /* If a working storage control module is available, it should be        */
 /* attached to the FatFs via a glue function rather than modifying it.   */
@@ -11,52 +11,40 @@
 #include "diskio.h"		/* Declarations of disk functions */
 
 /* Definitions of physical drive number for each drive */
-#define DEV_RAM		0	/* Example: Map Ramdisk to physical drive 0 */
-#define DEV_MMC		1	/* Example: Map MMC/SD card to physical drive 1 */
-#define DEV_USB		2	/* Example: Map USB MSD to physical drive 2 */
+// #define DEV_RAM		0	/* Example: Map Ramdisk to physical drive 0 */
+// #define DEV_MMC		1	/* Example: Map MMC/SD card to physical drive 1 */
+// #define DEV_USB		2	/* Example: Map USB MSD to physical drive 2 */
 
 
 /*-----------------------------------------------------------------------*/
 /* Get Drive Status                                                      */
 /*-----------------------------------------------------------------------*/
 
-DSTATUS disk_status (
-	BYTE pdrv		/* Physical drive nmuber to identify the drive */
-)
+DSTATUS disk_status( BYTE pdrv ) // Physical drive nmuber to identify the drive
 {
   return sd_status();
 }
-
-
 
 /*-----------------------------------------------------------------------*/
 /* Inidialize a Drive                                                    */
 /*-----------------------------------------------------------------------*/
 
-DSTATUS disk_initialize (
-	BYTE pdrv				/* Physical drive nmuber to identify the drive */
-)
+DSTATUS disk_initialize( BYTE pdrv ) // Physical drive nmuber to identify the drive
 {
   return sd_initialize();
 }
-
-
 
 /*-----------------------------------------------------------------------*/
 /* Read Sector(s)                                                        */
 /*-----------------------------------------------------------------------*/
 
-DRESULT disk_read (
-	BYTE pdrv,		/* Physical drive nmuber to identify the drive */
-	BYTE *buff,		/* Data buffer to store read data */
-	LBA_t sector,	/* Start sector in LBA */
-	UINT count		/* Number of sectors to read */
-)
+DRESULT disk_read( BYTE pdrv,    // Physical drive nmuber to identify the drive
+                   BYTE *buff,   // Data buffer to store read data
+                   LBA_t sector, // Sector address in LBA
+                   UINT count )  // Number of sectors to read
 {
   return sd_disk_read( buff, sector, count );
 }
-
-
 
 /*-----------------------------------------------------------------------*/
 /* Write Sector(s)                                                       */
@@ -64,12 +52,10 @@ DRESULT disk_read (
 
 #if FF_FS_READONLY == 0
 
-DRESULT disk_write (
-	BYTE pdrv,			/* Physical drive nmuber to identify the drive */
-	const BYTE *buff,	/* Data to be written */
-	LBA_t sector,		/* Start sector in LBA */
-	UINT count			/* Number of sectors to write */
-)
+DRESULT disk_write( BYTE pdrv,        // Physical drive nmuber to identify the drive
+                    const BYTE *buff, // Data to be written
+                    LBA_t sector,     // Sector address in LBA
+                    UINT count )      // Number of sectors to write
 {
   return sd_disk_write( buff, sector, count );
 }
@@ -81,12 +67,10 @@ DRESULT disk_write (
 /* Miscellaneous Functions                                               */
 /*-----------------------------------------------------------------------*/
 
-DRESULT disk_ioctl (
-	BYTE pdrv,		/* Physical drive nmuber (0..) */
-	BYTE cmd,		/* Control code */
-	void *buff		/* Buffer to send/receive control data */
-)
+DRESULT disk_ioctl( BYTE pdrv,    // Physical drive nmuber (0..)
+                    BYTE cmd,     // Control code
+                    void *buff )  // Buffer to send/receive control data
 {
-  return sd_disk_ioctl( cmd, buff );
+  return sd_disk_ioctl( cmd );
 }
 
